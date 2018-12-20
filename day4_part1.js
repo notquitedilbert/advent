525981a345cde372f76c7df9a89cd9a320cf1ce1
Andrew Shepherd <andrew.shepherd@dwp.gsi.gov.uk>
2018-12-20 15:52:48 +0000

    nearly done
    almost found the guard that sleeps the most

---
+++ day4_part1.js
@@ -0,0 +1,99 @@
+const readFile = require('./readFile');
+const guards = {};
+//* data example
+// "[1518-02-13 23:47] Guard #2797 begins shift"
+// "[1518-02-14 00:03] falls asleep"
+// "[1518-02-14 00:22] wakes up"
+// "[1518-02-14 00:41] falls asleep"
+// "[1518-02-14 00:53] wakes up"
+// "[1518-02-15 00:02] Guard #2267 begins shift"
+// "[1518-02-15 00:11] falls asleep"
+// "[1518-02-15 00:47] wakes up"
+
+// DONE we need to sort the data by the timestamp
+
+readFile('./day4_data.txt').then(lines => {
+    let currentGuardID = null;
+
+    // scan the line
+
+    const sortedData = lines.sort((a, b) => {
+        return getTime(a) - getTime(b)
+    })
+
+    // if the line contains GUARD then
+    // create a new guard object/or use an exsisting object
+    // if not , and we already have a guard id
+    // process the data for the current id
+    let id = null
+    lines.forEach(line => {
+        id = processLine(line, id);
+    });
+
+    processTimes(guards)
+    console.log(guards);
+
+})
+
+
+//* gurad object
+// id        - integer, the id of the gurad
+// totalTime - integer, the total of all the minutes (should really be a function)
+// [minutes] - array of integers, the calculated minutes from the wakes up/falls asleep times
+
+// get the date/time
+
+function getTime(line) {
+    // just a quick function to help sort the data
+    // remove some stuff we dont' need
+    let data = line.split(']')[0].replace(/-|:|\s|\[/g, '');
+
+    return data;
+
+}
+
+function processLine(line, id) {
+    // check the line for a ID number
+    // if there is one - use
+    // if there isn't - look for a new one
+    const regx = /#\d+/ // find a hash followed by at least one digit
+
+    let newId = regx.exec(line) ? regx.exec(line)[0] : false;
+
+    // see if actually found an ID, if not use the one supplied
+    if (!newId) { newId = id };
+
+    // check to see if we already have this guard
+    if (!guards[newId]) {
+        guards[newId] = { times: [] };
+    }
+    // todo this needs to be an object with another array in side
+    // push the data to the guard array // but not the lines with _guard_ in them
+
+    if (!regx.exec(line)) {
+        guards[newId].times.push(line);
+    };
+    return newId;
+}
+
+function processTimes(obj) {
+
+    // process the guards data
+    Object.keys(obj).forEach(function (key) {
+
+        // console.log(key, guards[key]);
+        obj[key].totalTime = 0
+
+        // need to step through in pairs
+        for (let index = 0; index < obj[key].times.length; index += 2) {
+            const sleep = obj[key].times[index];
+            const awake = obj[key].times[index + 1];
+
+            // console.log(getTime(awake) - getTime(sleep));
+            let time = (getTime(awake) - getTime(sleep));
+            obj[key].totalTime += time;
+        }
+
+    });
+
+}
